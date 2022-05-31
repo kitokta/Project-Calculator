52e55e97eb3a9595c79c73384284196b7cec2c59
Gabi Zanini <psicologia.plus@gmail.com>
2022-05-31 11:39:55 -0300

    Created operate and operator functions

---
+++ main.js
@@ -0,0 +1,34 @@
+const opMultiply = document.getElementById('multiply');
+const opAdd = document.getElementById('add');
+const opSubtract = document.getElementById('subtract');
+const opDivide = document.getElementById('divide');
+
+
+
+
+function add(num1, num2) {
+	return num1 + num2;
+};
+
+function divide(num1, num2) {
+	return num1 / num2;
+};
+
+function subtract(num1, num2) {
+	return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+  };
+  
+function operate(operator, num1, num2) {
+    if (operator === "+") return add(num1, num2);
+    else if (operator === "/") return divide(num1, num2);
+    else if (operator === "-") return subtract(num1, num2);
+    else return multiply(num1, num2);
+}
+
+
+
+
