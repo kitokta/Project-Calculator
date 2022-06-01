bf959fe82b68a8336001434fbde3ee7059a073bb
Gabi Zanini <psicologia.plus@gmail.com>
2022-06-01 12:12:02 -0300

    Removed split function and reworked logic

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ const equal = document.getElementById('equal');
 const dot = document.getElementById('dot');
 let num1 = 0;
 let num2 = 0;
-let op = ''
+let op = '';
+let opFinal = '';
 let numbers = [];
 let result = 0;
 
@@ -22,42 +23,41 @@ function reset() {
     num2 = '';
     numbers = [];
     result = '';
+    opFinal = '';
 }
 
 //função adicionar
 function add(num1, num2) {
-	return result = num1 + num2;
+	result = num1 + num2;
+    return display.textContent = `${result}`;
 };
 
 //função dividir
 function divide(num1, num2) {
-	return result = num1 / num2;
+	result = num1 / num2;
+    return display.textContent = `${result}`;
 };
 
 //função subtrair
 function subtract(num1, num2) {
-	return result = num1 - num2;
+	result = num1 - num2; 
+    return display.textContent = `${result}`;
 };
 
 //função multiplicar
 function multiply(num1, num2) {
-    return result = num1 * num2;
+    result = num1 * num2;
+    return display.textContent = `${result}`;
   };
 
  //função de chamar operação 
 function operate(op, num1, num2) {
-    if (op === "+") return add(num1, num2);
-    else if (op === "/") return divide(num1, num2);
-    else if (op === "-") return subtract(num1, num2);
+    if (opFinal === "+") return add(num1, num2);
+    else if (opFinal === "/") return divide(num1, num2);
+    else if (opFinal === "-") return subtract(num1, num2);
     else return multiply(num1, num2);
 };
 
-//guarda os dois numeros da conta
-function split() {
-    numbers = display.textContent.split(/[+-/*]+/);
-    return num1 = parseInt(numbers[0]),
-        num2 = parseInt(numbers[1]);
-}
 
 //reconhecimento do ponto na tela
 dot.addEventListener('click', () => {
@@ -66,16 +66,16 @@ dot.addEventListener('click', () => {
 
 //reconhecimento dos numeros na tela
 input.forEach(item => item.addEventListener('click', (event) => {
+    if (op!= '') num1 = parseInt(display.textContent), op = '', display.textContent = '';
     display.textContent = `${display.textContent}${event.target.value}`;
 }));
 
 //reconhecimento dos operadores na tela
 operator.forEach(item => item.addEventListener('click', (event) => {
-    if (event.target.id === 'multiply') op = 'x';
-    else if (event.target.id === 'add') op = '+';
-    else if (event.target.id === 'subtract') op = '-';
-    else if (event.target.id === 'divide') op = '/';
-    display.textContent = `${display.textContent}${op}`;
+    if (event.target.id === 'multiply') op = 'x', opFinal = 'x';
+    else if (event.target.id === 'add') op = '+', opFinal = '+';
+    else if (event.target.id === 'subtract') op = '-', opFinal = '-';
+    else if (event.target.id === 'divide') op = '/', opFinal = '/';
    }));
 
 //botão de limpar
@@ -89,12 +89,13 @@ equal.addEventListener('click', () => {
     //else if (op === '-') numbers = display.textContent.split('-');
     //else if (op === '/') numbers = display.textContent.split('/');
     //else if (op === 'x') numbers = display.textContent.split('x');
-    if (op === '' ) {
+    if (opFinal === '' ) {
         alert("Please insert an operator"); 
         reset();
         equal.removeEventListener('click');
     }
-    split();
-    operate(op, num1, num2)
-    display.textContent = `${result}`;
+    else {
+        num2 = parseInt(display.textContent);
+        operate(opFinal, num1, num2);
+    }
    });
